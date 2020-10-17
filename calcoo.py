ad80366c916159a2dd1e0b6c82ccba7cac16c6a8
jgabari <j.gabari.2017@alumnos.urjc.es>
2020-10-17 21:03:43 +0200

    empezado el archivo calcoo

---
+++ calcoo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+
+    def __init__(self, operando1, operando2):
+        self.operando1 = operando1
+        self.operando2 = operando2
+
+    def suma(self):
+        return self.operando1 + self.operando2
+
+    def resta(self):
+        return self.operando1 - self.operando2
+
+if __name__ == "__main__":
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    objeto = Calculadora(operando1, operando2)
+    if sys.argv[2] == "suma":
+        print(objeto.suma())
+    elif sys.argv[2] == "resta":
+        print(objeto.resta())
+    else:
+        sys.exit("Error: introduzca suma o resta")
