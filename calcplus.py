782f744e3f2bcb900dfa948234eb9bcbe6b12099
jgabari <j.gabari.2017@alumnos.urjc.es>
2020-10-18 19:45:51 +0200

    empezado y terminado el archivo calcplus

---
+++ calcplus.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoohija
+
+fich = open('fichero')
+lineas = fich.readlines()
+for linea in lineas:
+    lista = linea.split(',')
+    operacion = lista[0]
+    resultado = int(lista[1])
+    if operacion == "suma":
+        for operando in lista[2:]:
+            objeto = calcoohija.CalculadoraHija(resultado, int(operando))
+            resultado = objeto.suma()
+    elif operacion == "resta":
+        for operando in lista[2:]:
+            objeto = calcoohija.CalculadoraHija(resultado, int(operando))
+            resultado = objeto.resta()
+    elif operacion == "multiplica":
+        for operando in lista[2:]:
+            objeto = calcoohija.CalculadoraHija(resultado, int(operando))
+            resultado = objeto.multiplica()
+    elif operacion == "divide":
+        for operando in lista[2:]:
+            objeto = calcoohija.CalculadoraHija(resultado, int(operando))
+            resultado = objeto.divide()
+    else:
+        sys.exit("Error: introduzca suma, resta, multiplica o divide")
+    print(resultado)
