edd20bc399281aaa5c24ac350ef5fc8d02f5acf5
jgabari <j.gabari.2017@alumnos.urjc.es>
2020-10-18 13:20:14 +0200

    empezado y terminado el archivo calcoohija

--- calcoohija.py
+++ calcoohija.py
@@ -2,18 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import calcoo
 
-class Calculadora():
+class CalculadoraHija(calcoo.Calculadora):
 
     def __init__(self, operando1, operando2):
         self.operando1 = operando1
         self.operando2 = operando2
 
-    def suma(self):
-        return self.operando1 + self.operando2
+    def multiplica(self):
+        return self.operando1 * self.operando2
 
-    def resta(self):
-        return self.operando1 - self.operando2
+    def divide(self):
+        try:
+            return self.operando1 / self.operando2
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed")
 
 if __name__ == "__main__":
     try:
@@ -24,10 +28,14 @@ if __name__ == "__main__":
     except IndexError:
         sys.exit("Error: introduce 3 argumentos")
 
-    objeto = Calculadora(operando1, operando2)
+    objeto = CalculadoraHija(operando1, operando2)
     if sys.argv[2] == "suma":
         print(objeto.suma())
     elif sys.argv[2] == "resta":
         print(objeto.resta())
+    elif sys.argv[2] == "multiplica":
+        print(objeto.multiplica())
+    elif sys.argv[2] == "divide":
+        print(objeto.divide())
     else:
         sys.exit("Error: introduzca suma o resta")
